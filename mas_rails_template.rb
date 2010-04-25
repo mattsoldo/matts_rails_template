c992dfa5fa2dde1b06aa772f879ea6c79d8c0707
Matt Soldo <Matt@Heroku.com>
2010-04-25 09:44:43 -0700

    initial commit

---
+++ mas_rails_template.rb
@@ -0,0 +1,69 @@
+def heroku(command = {})
+  in_root do
+    if command.is_a?(Symbol)
+      log 'running', "heroku #{command}"
+      run "heroku #{command}"
+    else
+      command.each do |command, options|
+        log 'running', "heroku #{command} #{options}"
+        run("heroku #{command} #{options}")
+      end
+    end
+  end
+end
+
+
+generate :nifty_layout
+
+gem 'haml'
+gem 'will_paginate'
+gem 'formtastic'
+gem "factory_girl"
+gem 'shoulda'
+gem 'hassle'
+
+file ".gems", <<-END
+dry_scaffold  
+will_paginate  
+formtastic  
+factory_girl  
+inherited_resources --version 1.0.6
+shoulda
+hassle
+END
+
+## Create the database
+
+heroku :rake => "db:create"
+
+##############  commands #################
+
+git :init
+
+file ".gitignore", <<-END
+.DS_Store
+log/*.log
+tmp/**/*
+config/database.yml
+db/*.sqlite3
+END
+
+run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
+run "cp config/database.yml config/example_database.yml"
+
+git :add => ".", :commit => "-m 'initial commit'"
+
+generate :formtastic
+
+git :add => ".", :commit => "-m 'formtastic stylesheets'"
+
+generate :controller, "welcome index"
+route "map.root :controller => 'welcome'"
+git :rm => "public/index.html"
+
+git :add => ".", :commit => "-m 'adding welcome controller'"
+
+## Delpoy to Heroku
+heroku :create
+git :push => "heroku master"
+heroku :rake => "db:migrate"
